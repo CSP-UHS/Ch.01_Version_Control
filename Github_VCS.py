0ee3ffa9b328df0db1605f58e78c9d18107e5ccb
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-10 17:07:16 -0600

    Changed to "testing" branch

--- Github_VCS.py
+++ Github_VCS.py
@@ -17,7 +17,7 @@ Define Merging:
 Define Pushing:
 Define Pull Request:
 -----------------------------------------------
-Student 2: This is the master branch!
+Student 2: This is the testing branch!
 Full Name:
 Define Forking:
 Define Cloning:
