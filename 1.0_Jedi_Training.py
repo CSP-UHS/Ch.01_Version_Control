6254c0b39f758693138a93e5b1a86751c06b3d80
buiste <buiste@urbandaleschools.com>
2019-08-30 14:13:45 -0500

    Master branch

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:________Steven Bui________
 
 
 
-Define Forking: 
+Define Forking: copying a repository
 
-Define Cloning: 
+Define Cloning: making an exact copy of an application
 
-Define Branching:
+Define Branching: Branching is when you create a copy of the master branch but will not impact the master branch when edited
 
-Define Committing: 
+Define Committing: saving changes made to the branch
 
-Define Merging: 
+Define Merging: Taking the master branch and another branch and combining them
 
-Define Pushing:
+Define Pushing: transporting changes or commits to a remote repository
 
-Define Pull Request:
+Define Pull Request: submitting changes to a project to be validated into a main repository
 
 
 '''
