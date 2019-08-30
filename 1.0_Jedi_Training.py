5b6a0a87d63d24d1741407f6a08260734611ef60
barleypeg <barleypeg@urbandaleschools.com>
2019-08-30 14:15:13 -0500

    merging into the master
    # Conflicts:
    #	1.1_Github_VCS.py

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,26 @@
 '''
-Sign your name:________________
+Sign your name: Peggy Barley
 
 
 
-Define Forking: 
+Define Forking: A copy of an online read-only repository that
+can be worked on
 
-Define Cloning: 
+Define Cloning: Downloading the forked code to a local computer
+so the code can be worked on
 
-Define Branching:
+Define Branching:  A repository can also have multiple strands
+of development going on simultaneously
 
-Define Committing: 
+Define Committing: A checkpoint in the development of a project
 
-Define Merging: 
+Define Merging: Combining various versions of a code
 
-Define Pushing:
+Define Pushing: To push software into an environment that is publicly
+accessible to the rest of thw world
 
-Define Pull Request:
+Define Pull Request: Asking the project owner to pull their changes
+and merge them into the original code
 
 
 '''
