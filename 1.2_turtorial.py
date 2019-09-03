d51e892953ebb769b04e9a0c1bf5ef25a1eec085
Tristan Holman <holmantri@urbandaleschools.com>
2019-09-03 14:26:02 -0500

    Pokeball

--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -6,28 +6,35 @@ Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=
 
 '''
 import turtle
-yoda=turtle.Turtle()
-screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
-yoda.write('Sign Your Name Here',font=("Arial", 12, "normal"))
+
+pencil = turtle
+pencil.penup()
+
+pencil.pensize(12)
+pencil.goto(0,-150)
+pencil.pendown()
+pencil.circle(150)
+pencil.penup()
+pencil.goto(-150,0)
+pencil.pendown()
+pencil.pensize(10)
+pencil.goto(-50,0)
+pencil.penup()
+pencil.goto(50,0)
+pencil.pendown()
+pencil.goto(150,0)
+pencil.penup()
+pencil.pensize(3)
+pencil.goto(-25,0)
+pencil.penup()
+pencil.goto(0,-25)
+pencil.pendown()
+pencil.circle(25)
+pencil.penup()
+pencil.goto(0,-49)
+pencil.pendown()
+pencil.circle(49)
+pencil.penup()
 
 
 turtle.exitonclick() #Keeps pycharm window open
