9bd5759b7fdfa5578e694b44f05f0c6e17df76b2
hermoniuscapensis <phamjul@urbandaleschools.com>
2019-09-03 14:19:26 -0500

    Merge branch 'ChUno_Testing'
    # Conflicts:
    #	1.1_Github_VCS.py

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Julie Pham
 
 
 
-Define Forking: 
+Define Forking: saving someone else's repository onto your own github account
 
-Define Cloning: 
+Define Cloning: downloading it onto your computer
 
-Define Branching:
+Define Branching: making different copies of one program with different code
 
-Define Committing: 
+Define Committing: saving new code
 
-Define Merging: 
+Define Merging: combining new changes in a repository with the original repository
 
-Define Pushing:
+Define Pushing: pushing code to your github account/to the forked remote repo
 
-Define Pull Request:
+Define Pull Request: requesting to merge the original repository with yours (the one with new changes)
 
 
 '''
