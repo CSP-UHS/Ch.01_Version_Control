1910cf1e660fb282d591e5fbea75a0d83a90ea35
Cal Watson <watsoncal@urbandaleschools.com>
2019-09-04 18:46:49 -0500

    Signed Name and Wrote Definitions

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Cal Watson
 
 
 
-Define Forking: 
+Define Forking: copying a shared repository
 
-Define Cloning: 
+Define Cloning: copying a forked repository onto a local repository/computer
 
-Define Branching:
+Define Branching: duplicating a file to independently test changes to a code
 
-Define Committing: 
+Define Committing: recording/saving changes to a file and adding a description
 
-Define Merging: 
+Define Merging: replacing a branch of code with a test branch
 
-Define Pushing:
+Define Pushing: sending the changes made on a file from a local to remote repository
 
-Define Pull Request:
+Define Pull Request: asking the owner of a file to merge changes into the original code
 
 
 '''
