3a084e94c56f9b4ce95c3736a2647e72826332dc
kimmerdra <kimmerdra@urbandaleschools.com>
2019-09-05 13:43:33 -0500

    2nd blah

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -3,19 +3,19 @@ Sign your name: Drake Kimmer
 
 
 
-Define Forking: 
+Define Forking: this means to pick up someones code and put it on your computer and you can make you own changes
 
-Define Cloning: 
+Define Cloning: where we have our own respository on your computer
 
-Define Branching:
+Define Branching: this is where you can make changes on the same page of code but not on your orgininal
 
-Define Committing: 
+Define Committing: this is comfiriming the line of code kinda like saving it but not really
 
-Define Merging: 
+Define Merging: this is where you can merge all of your branhes in to one page of code
 
-Define Pushing:
+Define Pushing: this is pushing your code to GitHub
 
-Define Pull Request:
+Define Pull Request: this is pulling your changes to the code in GitHub
 
 
 '''
