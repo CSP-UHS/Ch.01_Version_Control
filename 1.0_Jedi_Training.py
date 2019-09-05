111451db8f9799c55697cda3fcd24edbc2192e71
halsteaddan <halsteaddan@urbandaleschools.com>
2019-09-05 13:13:35 -0500

    Finished.

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Danny Halstead
 
 
 
 Define Forking: 
-
+Copying a repository from the organization github to your own github acount.
 Define Cloning: 
-
+Taking it from your github account into pycharm.
 Define Branching:
-
+making multiple paths in a file so you can test new things without messing up what you have.
 Define Committing: 
-
+Making your current (autosaved) changes permanent.
 Define Merging: 
-
+Taking your multiple branches for testing things and combining them back into one.
 Define Pushing:
-
+Pushing your current code back into your own github account.
 Define Pull Request:
-
+Sending the code from your github account back to the organization owner.
 
 '''
