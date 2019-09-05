e34f81d3601a6fcc5438a517ae0935f808e13eb2
hermonm <hermonm@urbandaleschools.com>
2019-09-05 13:16:06 -0500

    merging my second branch
    # Conflicts:
    #	1.1_Github_VCS.py

--- 1.1_Github_VCS.py
+++ 1.1_Github_VCS.py
@@ -200,7 +200,7 @@ Each student has 10 lines of code to play with!
 ------------------Student 20------------------
 This is the master branch
 Marc Hermon is awesome
-Go Cardinals!!!!!
+Go Cardinals!!!!!!
 
 
 
