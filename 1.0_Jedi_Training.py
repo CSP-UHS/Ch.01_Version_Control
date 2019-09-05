a13e760d9bdaf44631fbffd7d28546cf19d8bc95
Kenny <floryken@urbandaleschools.com>
2019-09-05 13:36:09 -0500

    Update

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -3,19 +3,19 @@ Sign your name:Kenny Flory
 
 
 
-Define Forking: 
+Define Forking: A cop of a repository that the person can edit but doesnt change original
 
-Define Cloning: 
+Define Cloning: Copy that goes on computer
 
-Define Branching:
+Define Branching: Instruction that makes computer start a different branch
 
-Define Committing: 
+Define Committing: Save
 
-Define Merging: 
+Define Merging: Combining two different saves of the same program
 
-Define Pushing:
+Define Pushing: Sending something out to github
 
-Define Pull Request:
+Define Pull Request: Asking teacher to pull code in
 
 
 '''
