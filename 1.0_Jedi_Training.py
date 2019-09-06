32714ca8537a7b0f0046f853297420bc7b2fa64b
cobbzac <cobbzac@urbandaleschools.com>
2019-09-06 14:01:28 -0500

    my first commit

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Zach Cobb
 
 
 
-Define Forking: 
+Define Forking: to grab a file from another place and use it
 
-Define Cloning: 
+Define Cloning: make a copy of something
 
-Define Branching:
+Define Branching: making another seperate code to see how a change affects is before applying it
 
-Define Committing: 
+Define Committing: Saving a file in your python
 
-Define Merging: 
+Define Merging: when you combine two different branches and the changes you made in the other branch
 
-Define Pushing:
+Define Pushing: Uploads the file to github
 
-Define Pull Request:
+Define Pull Request: submitting your work to someone
 
 
 '''
--- 1.1_Github_VCS.py
+++ 1.1_Github_VCS.py
@@ -119,67 +119,10 @@
 
 
 ------------------Student 13------------------
-import turtle
-tina = turtle.Turtle()
-tina.shape('turtle')
+Zach Cobb
+Father of four
+Proud owner of MIXATRON 9000
 
-tina.penup()
-tina.right(90)
-tina.forward(180)
-tina.right(90)
-tina.forward(180)
-tina.right(90)
-
-tina.pendown()
-tina.pencolor("red")
-tina.forward(300)
-tina.right(90)
-tina.pencolor("orange")
-tina.forward(300)
-tina.right(90)
-tina.pencolor("yellow")
-tina.forward(250)
-tina.right(90)
-tina.pencolor("green")
-tina.forward(250)
-tina.right(90)
-tina.pencolor("blue")
-tina.forward(251)
-tina.right(135)
-tina.pencolor("indigo")
-tina.forward(130)
-tina.right(45)
-tina.pencolor("violet")
-tina.forward(110)
-tina.right(62)
-tina.pencolor("red")
-tina.forward(105)
-
-tina.penup()
-tina.backward(90)
-tina.right(180)
-tina.forward(17)
-tina.right(28)
-
-tina.pendown()
-tina.pencolor("orange")
-tina.forward(90)
-tina.right(38)
-tina.pencolor("yellow")
-tina.forward(85)
-tina.backward(85)
-tina.left(128)
-tina.pencolor("green")
-tina.forward(110)
-tina.right(37)
-tina.pencolor("blue")
-tina.forward(112)
-tina.backward(112)
-tina.left(127)
-tina.pencolor("indigo")
-tina.forward(90)
-
-turtle.exitonclick()
 
 
 
