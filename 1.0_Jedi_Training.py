383142200582a39b635bb71ec8c44edf68d0e89c
burkheadlil <burkheadlil@urbandaleschools.com>
2019-09-06 13:55:53 -0500

    2nd commit

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -3,20 +3,21 @@ Sign your name: Lily
 
 
 
-Define Forking: 
+Define Forking: taking a code from an organization and taking it to your account allowing you to work on it
 
 Define Cloning: It's like downloading
 
 Define Branching: When you want to test something but you don't want it to affect your original code you create a
     branch to see if it works
 
-Define Committing: 
+Define Committing: It's like saving
 
-Define Merging: 
+Define Merging: take a branch that your working on and your original branch and combining them together
 
-Define Pushing:
+Define Pushing: you take what your working on and "push" it to your account that's somewhere else
+    this allows you to later do a pull request
 
-Define Pull Request:
+Define Pull Request: sending your code to some organization and asking them to merge your code with their original
 
 
 '''
