8d06661d32d1210406a7b2393c54d5d86265760f
halsteaddan <halsteaddan@urbandaleschools.com>
2019-09-06 14:18:50 -0500

    -Removed Herobrine

--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -1,20 +1,17 @@
-import turtle #Without turtle there isn't ocean
-DeepSeaSnapper=turtle.Turtle() #the best turtles are underwater
+import turtle # without turtle there isn't ocean
+DeepSeaSnapper=turtle.Turtle() # the best turtles are underwater
 screen=turtle.Screen() # makes a screen object
-screen.bgcolor('white') # colors the background white
+screen.bgcolor('white') # colors the background
 
 DeepSeaSnapper.pensize(3) # width of pen line
 DeepSeaSnapper.speed(10)  # speed of drawing
-DeepSeaSnapper.color("Black")
+DeepSeaSnapper.color("Black") # color of line
 
 DeepSeaSnapper.pendown()
 
 DeepSeaSnapper.penup()
 
-
-
 DeepSeaSnapper.pencolor('#Purple')
-DeepSeaSnapper.write('Sign Your Name Here',font=("ComicSans", 12, "normal"))
-
+DeepSeaSnapper.write('Danny H',font=("ComicSans", 12, "normal"))
 
 turtle.exitonclick() #Keeps pycharm window open
