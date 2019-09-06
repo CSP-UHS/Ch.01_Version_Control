b836117ba235029d6bf7fd1be2069e60fbb5ba86
alexanderrow <alexanderrow@urbandaleschools.com>
2019-09-06 14:23:37 -0500

    this is fishing

--- 1.1_Github_VCS.py
+++ 1.1_Github_VCS.py
@@ -12,7 +12,7 @@ I love Fishing
 
 yeet
 fish
-
+yeet
 
 
 
--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -6,28 +6,20 @@ Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=
 
 '''
 import turtle
-yoda=turtle.Turtle()
+fish=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
 screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
-yoda.write('Sign Your Name Here',font=("Arial", 12, "normal"))
+fish.pensize(3) # width of pen line
+fish.speed(2)  # speed of drawing. Go fast to not waste time.
+fish.color("#0000A0")
+fish.circle(85)
+fish.circle(85,-85)
+fish._rotate(-45)
+fish.forward(150)
+fish._rotate(135)
+
+
+fish.write('Rowdy Alexander',font=("Arial", 12, "normal"))
 
 
 turtle.exitonclick() #Keeps pycharm window open
