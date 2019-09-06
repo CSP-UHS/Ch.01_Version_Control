3ee48f5022d1b55948cb556e07a65588decfc1e5
halsteaddan <halsteaddan@urbandaleschools.com>
2019-09-06 14:16:51 -0500

    Adding Stuff.

--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -3,25 +3,18 @@ DeepSeaSnapper=turtle.Turtle() #the best turtles are underwater
 screen=turtle.Screen() # makes a screen object
 screen.bgcolor('white') # colors the background white
 
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
-yoda.write('Sign Your Name Here',font=("Arial", 12, "normal"))
+DeepSeaSnapper.pensize(3) # width of pen line
+DeepSeaSnapper.speed(10)  # speed of drawing
+DeepSeaSnapper.color("Black")
+
+DeepSeaSnapper.pendown()
+
+DeepSeaSnapper.penup()
+
+
+
+DeepSeaSnapper.pencolor('#Purple')
+DeepSeaSnapper.write('Sign Your Name Here',font=("ComicSans", 12, "normal"))
 
 
 turtle.exitonclick() #Keeps pycharm window open
