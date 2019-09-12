62d3ec21d3e0db6ec17c01c23f099f47e8bec091
calebhews <calebhews@gmail.com>
2019-09-12 13:59:43 -0500

    Jedi Training #1

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Caleb Hews
 
 
 
-Define Forking: 
+Define Forking: Forking is taking a repository from an organization
 
-Define Cloning: 
+Define Cloning: cloning is when you transfer a repository from your Github account
 
-Define Branching:
+Define Branching: Creating multiple outcomes from the original code
 
-Define Committing: 
+Define Committing: Committing is when you committ to the changes you have made to the original
 
-Define Merging: 
+Define Merging: Merging is when the head of an organization takes your code and merges is with the master code
 
-Define Pushing:
+Define Pushing: Pushing is when you push a code from Python to your Github account
 
-Define Pull Request:
+Define Pull Request: a pull request is when you send your code to the organization leader
 
 
 '''
