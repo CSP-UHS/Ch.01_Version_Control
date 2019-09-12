978481e35bc4853e556ea9ede034dafb34efc2d6
alexanderrow <alexanderrow@urbandaleschools.com>
2019-09-12 14:15:25 -0500

    this is a half completed fish

--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -10,13 +10,29 @@ fish=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
 screen.bgcolor('black') # colors the screen
 fish.pensize(3) # width of pen line
-fish.speed(2)  # speed of drawing. Go fast to not waste time.
+fish.speed(7)  # speed of drawing. Go fast to not waste time.
 fish.color("#0000A0")
 fish.circle(85)
 fish.circle(85,-85)
 fish._rotate(-45)
 fish.forward(150)
 fish._rotate(135)
+fish._rotate(90)
+fish.forward(250)
+fish._rotate(-130)
+fish.forward(160)
+fish._rotate(45)
+fish.penup()
+fish.forward(96)
+fish.pendown()
+fish.circle(13)
+fish._rotate(-90)
+fish.penup()
+fish.forward(56)
+fish.pendown()
+fish.circle(-35,40)
+
+
 
 
 fish.write('Rowdy Alexander',font=("Arial", 12, "normal"))
