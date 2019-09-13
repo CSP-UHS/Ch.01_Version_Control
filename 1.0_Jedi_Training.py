78a8f83cda49b842ace02f263ac6f8ff0315e6a5
smithdyl <smithdyl@urbandaleschools.com>
2019-09-13 14:06:24 -0500

    Bet

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -3,19 +3,19 @@ Sign your name:Dylan Smith
 
 
 
-Define Forking: 
+Define Forking: Taking the program from a page onto your computer
 
-Define Cloning: 
+Define Cloning: cloning the already existing program for you to edit
 
-Define Branching:
+Define Branching: area inside of the program to add your stuff
 
-Define Committing: 
+Define Committing: making your code apar tof the base code
 
-Define Merging: 
+Define Merging: putting your program together with other programs to make a bigger program
 
-Define Pushing:
+Define Pushing: putting your code out to the world
 
-Define Pull Request:
+Define Pull Request: a request to have our code added to the master code
 
 
 '''
