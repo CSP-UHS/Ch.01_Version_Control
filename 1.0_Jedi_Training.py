4c9d6079723776038ed4f296a5bca9efd36b1da6
stamerzev <stamerzev@urbandaleschools.com>
2020-09-04 09:56:26 -0500

    My 1st commitment

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,11 +1,11 @@
 '''
-Sign your name:________________
+Sign your name: Zev Stamer
 
 
 
-Define Forking: 
+Define Forking: Stab the food and bring it to your own account. On git hub. Off teachers organization.
 
-Define Cloning: 
+Define Cloning: Downloading or Cloning something into the python folder, or downloading it.
 
 Define Branching:
 
