11462b467d8f7d16d5ea3e6bf5f0a9e10abf0041
Nathan topolinski <Topolinskinat@urbandaleschools.com>
2020-09-04 10:53:58 -0500

    jedi traing branching and commiting

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -7,9 +7,9 @@ Define Forking: copuing  code from one accoint to your account
 
 Define Cloning: makeing another copy from your remote account to your local account
 
-Define Branching:
+Define Branching: creating a copy of the master branch
 
-Define Committing: 
+Define Committing: a more complacated save
 
 Define Merging: 
 
