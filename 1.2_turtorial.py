bfbfcd1a921b0252deb558b08277f68a3ff52ce0
Ryan Pretzel <muetzelrya@urbandaleschools.com>
2020-09-10 08:16:23 -0500

    turtorial commit

--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -8,28 +8,71 @@ Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=
 
 '''
 import turtle
-yoda=turtle.Turtle()
-screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
 
+def draw_circle(turtle, color, size, x, y):
+    turtle.penup()
+    turtle.color(color)
+    turtle.fillcolor(color)
+    turtle.goto(x,y)
+    turtle.begin_fill()
+    turtle.pendown()
+    turtle.circle(size)
+    turtle.penup()
+    turtle.end_fill()
+    turtle.pendown()
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
-turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
+def draw_rectangle(turtle, color, x1, y1, x2, y2):
+    turtle.penup()
+    turtle.color(color)
+    turtle.fillcolor(color)
+    turtle.goto(x1,y1)
+    turtle.begin_fill()
+    turtle.pendown()
+    turtle.goto(x1,y2)
+    turtle.goto(x2,y2)
+    turtle.goto(x2,y1)
+    turtle.goto(x1,y1)
+    turtle.penup()
+    turtle.end_fill()
+    turtle.pendown()
+
+tom = turtle.Turtle()
+tom.shape("turtle")
+tom.speed(500)
+
+draw_circle(tom, "yellow", 200, 0, -12)  #makes outer circle
+draw_circle(tom, "white", 185, 0, 0)
+draw_rectangle(tom, "yellow", -200, 180, -30, 190)  #makes horizontal line
+draw_rectangle(tom, "yellow", -5, 20, 5, 380)    #makes vertical line
+draw_rectangle(tom, "white", -5, 360, -15, 400)   #makes the little space at the top
+
+tom.penup() #making the bottom of the "Q"
+tom.color("yellow")
+tom.fillcolor("yellow")
+tom.goto(10, 0)
+tom.begin_fill()
+tom.pendown()
+tom.goto(150, -51)
+tom.goto(225, -61)
+tom.goto(201, -68)
+tom.goto(152,-70)
+tom.goto(100, -60)
+tom.goto(30, -30)
+tom.goto(0,-25)
+tom.goto(-50, -30)
+tom.goto(-58, -28)
+tom.goto(-55,-20)
+tom.goto(-20,0)
+tom.goto(10,0)
+tom.penup()
+tom.end_fill()
+
+tom.penup()
+tom.goto(0,-120)
+tom.color('black')
+tom.write("Ryan Muetzel", align="center", font=(None, 16, "bold"))
+tom.goto(420,-400)
+tom.write("RIP TOM 2.0", align="center", font=(None, 12))  #hehe
+tom.goto(0,-160)
+
+turtle.Screen().exitonclick()
