bf7816cb5ad3941f3d5107be253c572ef09e7e95
waltersjac <waltersjac@urbandaleschools.com>
2020-09-14 09:11:55 -0500

    1st test commit

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:Jacob Walters
 
 
 
-Define Forking: 
+Define Forking: making a copy of a repository on to your account
 
-Define Cloning: 
+Define Cloning: making a copy of a repository onto the computer
 
-Define Branching:
+Define Branching: making an copy of code you can edit and change from master
 
-Define Committing: 
+Define Committing: a hard save of a version in a bracn you can go back to
 
-Define Merging: 
+Define Merging: making to branches of code come together
 
-Define Pushing:
+Define Pushing: overridng a save of code from your computer to github account
 
-Define Pull Request:
+Define Pull Request: a request to edit base code from organization
 
 
 '''
