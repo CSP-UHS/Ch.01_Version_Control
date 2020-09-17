cf03691b50b123bb68ce420bca06b933104fbbda
dautom21 <dautom@urbandaleschools.com>
2020-09-17 10:46:39 -0500

    First commit I think, I hope I'm doing it right.

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:Tom Dau
 
 
 
-Define Forking: 
+Define Forking: When someone makes a copy of a shared repository
 
-Define Cloning: 
+Define Cloning: The repository is cloned and can be worked on in the local computer
 
-Define Branching:
+Define Branching: Multiple strands of development going on simultaneously in a repository
 
-Define Committing: 
+Define Committing: A checkpoint in the development of a project; A hard save
 
-Define Merging: 
+Define Merging: Putting a testing branch back into the master branch
 
-Define Pushing:
+Define Pushing: Pushes all commits to GitHub accounts
 
-Define Pull Request:
+Define Pull Request: A request from students for the teacher to pull into the main repository
 
 
 '''
