4ecf7f8739ff9a894e2960985486acff35aead9f
lopezsantiagoger <lopezsantiagoger@urbandaleschools.com>
2020-10-02 13:47:22 -0500

    inviernov2

--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -10,26 +10,56 @@ Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=
 import turtle
 yoda=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
+screen.bgcolor('navy') # colors the screen
+yoda.pensize(5) # width of pen line
+yoda.speed(5)  # speed of drawing. Go fast to not waste time.
+yoda.color("#000000")
 yoda.penup()
-yoda.setpos(50,185) #right ear
+yoda.setpos(-20,150)#first shape
 yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
+yoda.goto(-20,150)
+yoda.goto(10,120)
 yoda.penup()
-yoda.setpos(-50,185)  #left ear
+yoda.setpos(10,120)
 yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
+yoda.goto(10,120)
+yoda.goto(-120,-20)
 yoda.penup()
-yoda.setpos(200,-300)
+yoda.setpos(-120,-20)
 yoda.pendown()
-yoda.pencolor('#00FF00')
+yoda.goto(-120,-20)
+yoda.goto(-120,50)
+yoda.penup()
+yoda.setpos(-120,50)
+yoda.pendown()
+yoda.goto(-120,50)
+yoda.goto(-20,150)
+yoda.penup()
+
+yoda.setpos(-65,-10)#second shape
+yoda.pendown()
+yoda.goto(-65,-10)
+yoda.goto(-165,-110)
+yoda.penup()
+yoda.setpos(-165,-110)
+yoda.pendown()
+yoda.goto(-165,-110)
+yoda.goto(-165,-180)
+yoda.penup()
+yoda.setpos(-165,-180)
+yoda.pendown()
+yoda.goto(-165,-180)
+yoda.goto(-35,-40)
+yoda.penup()
+yoda.setpos(-35,-40)
+yoda.pendown()
+yoda.goto(-35,-40)
+yoda.goto(-65,-10)
+yoda.penup()
+yoda.pencolor('#000000')
+yoda.setpos(200,-200)
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+
+yoda.write('Gerardo Lopez',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
