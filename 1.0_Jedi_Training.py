af05c0a6548f28ae346886f61e3357178d8c580a
lopezsantiagoger <lopezsantiagoger@urbandaleschools.com>
2020-10-05 10:01:35 -0500

    commitanswer

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Gerardo Lopez
 
 
 
 Define Forking: 
- Copying from an organization
-Define Cloning: 
-
+    Copying from an organization
+Define Cloning:
+    Putting the code into your computer
 Define Branching:
-
+    Having multiple strands of development going on simultaneously
 Define Committing: 
-
+    Saving the code
 Define Merging: 
-
+    Merging branches of code together
 Define Pushing:
-
+    Putting the edited code into your github account
 Define Pull Request:
-
+    Asking permission for the edited code to merge with the main repository
 
 '''
