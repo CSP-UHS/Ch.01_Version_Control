fdcbd39b1d2ccfe0d18d8f9af0cf86234496aeca
getzbry <getzbry@urbandaleschools.com>
2020-10-06 09:34:24 -0500

    Finished Turtorial

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -7,15 +7,15 @@ Define Forking: Taking an assignment and making it your own
 
 Define Cloning: getting the code to your folder so it is accessible
 
-Define Branching:
+Define Branching: its lke a test file, you test code here
 
-Define Committing: 
+Define Committing: a checkpoint in the development of a project
 
-Define Merging: 
+Define Merging: when you merge some testing branches into your real code
 
-Define Pushing:
+Define Pushing:pushing our project onto our github account
 
-Define Pull Request:
+Define Pull Request: asking the owner to pull their changes and merge them into the original code
 
 
 '''
--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -11,24 +11,35 @@ import turtle
 goal=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
 screen.bgcolor('white') # colors the screen
-goal.pensize(3) # width of pen line
-goal.speed(10)  # speed of drawing. Go fast to not waste time.
-goal.color("#00FF00")
-goal.penup(-200,0) #horizon
-goal.setpos()
-goal.pendown(-200,0)
-goal.goto(200-,0)
-goal.goto()
-goal.penup()
-goal.setpos()
+goal.pensize(5) # width of pen line
+goal.speed(5)  # speed of drawing. Go fast to not waste time.
+goal.color("black")
+
+ #horizon
 goal.pendown()
-goal.goto()
-goal.goto()
-goal.penup()
-goal.setpos()
+goal.goto(-200,0)
+goal.setpos(200,0)
 goal.pendown()
-goal.pencolor('#00FF00')
+goal.goto(200,0)
+#goal
+goal.penup()
+goal.goto(-75,0)
+goal.pendown()
+goal.color("grey")
+goal.goto(-75,50)
+goal.goto(75,50)
+goal.goto(75,0)
+#ball
+goal.penup()
+goal.goto(-90,-130)
+goal.color("black")
+goal.pendown()
+goal.circle(50)
+goal.penup()
+goal.goto(0,-50)
 
 
-yoda.write('Bryce Getz',font=("Arial", 16, "normal")) # signs your name to your art
+
+
+goal.write('Bryce Getz',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
