9c4dbbc2ec0423629ad65c8f882939105d40e135
hmarlal1 <hmarlal1@urbandaleschools.com>
2020-10-25 21:01:18 -0500

    done :D

--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -8,27 +8,82 @@ Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=
 
 '''
 import turtle
-yoda=turtle.Turtle()
-screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
+
+yoda = turtle.Turtle()
+
+yoda.speed(20)
+yoda.color('peach puff')
 yoda.penup()
-yoda.setpos(50,185) #right ear
+yoda.goto(200,200)
 yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
+yoda.begin_fill()
+yoda.goto(200,-200)
+yoda.goto(-200,-200)
+yoda.goto(-200,200)
+yoda.end_fill()
+
+yoda.color('orange')
+
+yoda.shape('turtle')
 yoda.penup()
-yoda.setpos(-50,185)  #left ear
+yoda.goto(-200,200)
 yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
+yoda.begin_fill()
+yoda.goto(200,200)
+yoda.goto(200,0)
+yoda.goto(150,0)
+yoda.goto(150,50)
+yoda.goto(100,50)
+yoda.goto(100,100)
+yoda.goto(0,100)
+yoda.goto(0,50)
+yoda.goto(-50,50)
+yoda.goto(-50,0)
+yoda.goto(-200,0)
+yoda.end_fill()
+
 yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
+yoda.color('white')
+yoda.goto(-150,-50)
+yoda.begin_fill()
+yoda.goto(-150,0)
+yoda.goto(-100,0)
+yoda.goto(-100,-50)
+yoda.end_fill()
+
+yoda.penup()
+yoda.goto(150,0)
+yoda.begin_fill()
+yoda.goto(150,-50)
+yoda.goto(100,-50)
+yoda.goto(100,0)
+yoda.end_fill()
+
+yoda.penup()
+yoda.color('green')
+yoda.goto(100,0)
+yoda.begin_fill()
+yoda.goto(100,-50)
+yoda.goto(50,-50)
+yoda.goto(50,0)
+yoda.end_fill()
+
+yoda.penup()
+yoda.goto(-50,0)
+yoda.begin_fill()
+yoda.goto(-50,-50)
+yoda.goto(-100,-50)
+yoda.goto(-100,0)
+yoda.end_fill()
+
+yoda.penup()
+yoda.color('light pink')
+yoda.goto(-50,-100)
+yoda.begin_fill()
+yoda.goto(50,-100)
+yoda.goto(50,-150)
+yoda.goto(-50,-150)
+yoda.end_fill()
 
 
 yoda.write('James Hmar',font=("Arial", 16, "normal")) # signs your name to your art
