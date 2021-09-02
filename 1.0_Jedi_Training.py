45bbb9214a0cf2f6d5034ac8db3f45cb94fcf1c7
toricden <toricden@urbandaleschools.com>
2021-09-02 09:51:35 -0500

    Testing #42069

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:Denis Toric
 
 
 
-Define Forking: 
+Define Forking: makes a copy of a repository
 
-Define Cloning: 
+Define Cloning: an exact of code copy downloaded to your computer
 
-Define Branching:
+Define Branching: Different versions of your code that
 
-Define Committing: 
+Define Committing: Saving the code between branches on your local computer
 
-Define Merging: 
+Define Merging: Combining multiple files of code to enter one
 
-Define Pushing:
+Define Pushing: Sending PyCharm code to a GitHub repository
 
-Define Pull Request:
+Define Pull Request: Way to edit or share code to a singular file
 
 
 '''
