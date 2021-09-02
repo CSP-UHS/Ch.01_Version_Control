7074ef3b07ef89b1aae244603ff93a7269291937
bakerkai <bakerkai@urbandaleschools.com>
2021-09-02 09:56:24 -0500

    Jedi training

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,23 @@
 '''
-Sign your name:________________
+Sign your name:____Kai _____
 
 
 
 Define Forking: 
-
+Making a full copy of the master branch and messing around with it without affecting the master branch
 Define Cloning: 
-
+Allows you to download the master file and work on it and when you get the changes that you want to push you can push
+it to the master
 Define Branching:
-
+Taking a side project of the master file and can be used for working on projects and it can be merged with the full
+master later on
 Define Committing: 
-
+Basically like saving the file with a small change
 Define Merging: 
-
+Putting the small changes into the main branch and making sure that it works with no conflicts
 Define Pushing:
-
+Pushing it into the master file
 Define Pull Request:
-
+Asking the master to pull the file into the main system
 
 '''
