2b37a45a024139487994ffae9b061cd44a1ae2e7
Joseph Jarman <57240500+Jarmander333@users.noreply.github.com>
2021-09-02 10:06:07 -0500

    Update 1.0_Jedi_Training.py

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:Joseph JArman
 
 
 
-Define Forking: 
+Define Forking: Copying a repository (a folder) to your own personal cloud account
 
-Define Cloning: 
+Define Cloning: Downloading a full copy
 
-Define Branching:
+Define Branching: Creating a "second repository" (alternate versions) in the existing repository
 
-Define Committing: 
+Define Committing: creating a "save point" so to speak of your code at numerous states
 
-Define Merging: 
+Define Merging: Merging a requested pull request into the master branch of the master repository
 
-Define Pushing:
+Define Pushing: Uploading your local repository to the remote server
 
-Define Pull Request:
+Define Pull Request: Asking to include you code with the original code
 
 
 '''
