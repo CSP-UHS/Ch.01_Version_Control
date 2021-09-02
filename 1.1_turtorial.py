b87e8d7219f3e3dcfd2104c115d13dc4a1346bdc
bakerkai <bakerkai@urbandaleschools.com>
2021-09-02 10:34:13 -0500

    weird red thing drawing 1

--- 1.1_turtorial.py
+++ 1.1_turtorial.py
@@ -11,25 +11,38 @@ import turtle
 yoda=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
 screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
+yoda.pensize(100) # width of pen line
 yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
+yoda.color("#FF0000")
 yoda.circle(100)  #head
+yoda.fillcolor("#00FF00")
 yoda.penup()
 yoda.setpos(50,185) #right ear
 yoda.pendown()
 yoda.goto(200,210)
 yoda.goto(88,145)
 yoda.penup()
+yoda.setpos(50,100) #test for bottom right thing
+yoda.pendown()
+yoda.goto(220,35)
+yoda.goto(35,1)
+yoda.penup()
 yoda.setpos(-50,185)  #left ear
 yoda.pendown()
 yoda.goto(-200,210)
 yoda.goto(-88,145)
 yoda.penup()
+yoda.setpos(-50,100) #attempt for bottom left thing
+yoda.pendown()
+yoda.goto(-200,150)
+yoda.goto(-88,100)
+yoda.penup()
 yoda.setpos(200,-300)
 yoda.pendown()
-yoda.pencolor('#00FF00')
-
-
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+yoda.pencolor('#FFFFFF')
+yoda.penup()
+yoda.goto(12,-65)
+#onclick(yoda.write('Kai Baker',font=("Arial", 16, "normal")))
+yoda.write('Kai Baker',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
+
