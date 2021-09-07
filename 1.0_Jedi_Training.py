8d8cb8a63f4b7eab1fee11071e4989ceffec8884
hubbyada <hubbyada@urbandaleschools.com>
2021-09-07 10:07:09 -0500

    idk

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Adan Hubby
 
 
 
-Define Forking: 
+Define Forking: Copying a repository
 
-Define Cloning: 
+Define Cloning: Duplicating and downloading everything in the repository
 
-Define Branching:
+Define Branching: A version of the repository with different code
 
-Define Committing: 
+Define Committing: Saving changes to GitHub
 
-Define Merging: 
+Define Merging: Combining different lines of code from different files
 
-Define Pushing:
+Define Pushing: Transferring commits from your repository to the remote repository
 
-Define Pull Request:
+Define Pull Request: Notifies the person you sent it to that you have changes that are completed and need to be grabbed
 
 
 '''
--- 1.1_turtorial.py
+++ 1.1_turtorial.py
@@ -8,28 +8,72 @@ Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=
 
 '''
 import turtle
-yoda=turtle.Turtle()
+bob = turtle.Turtle()
+turtle.bgcolor("Light Blue")
+turtle.speed(10)
+bob.penup()
+bob.goto(-350,35)
+bob.color("wheat4")
+bob.width(20)
+bob.pendown()
+bob.begin_fill()
+bob.right(90)
+bob.forward(360)
+bob.left(90)
+bob.forward(200)
+bob.left(90)
+number_list = [1,2,3,4,5,6,7,8,9]
+for number in number_list:
+    bob.right(5)
+    bob.forward(40)
+bob.right(45)
+bob.forward(80)
+bob.left(90)
+bob.forward(40)
+bob.left(90)
+bob.forward(425)
+bob.end_fill()
+bob.color("Green")
+bob.right(180)
+bob.width(30)
+bob.forward(425)
+bob.penup()
+bob.left(90)
+bob.forward(40)
+bob.width(10)
+bob.color("Red")
+bob.left(90)
+bob.forward(40)
+bob.right(45)
+bob.pendown()
+bob.begin_fill()
+number_list = [1,2,3,4,5,6,7,8,9]
+for number in number_list:
+    bob.right(5)
+    bob.forward(15)
+bob.right(15)
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+for number in number_list:
+        bob.right(6)
+        bob.forward(5)
+number_list = [1,2,3,4,5,6,7,8,9,10]
+for number in number_list:
+    bob.right(8.5)
+    bob.forward(15)
+bob.end_fill()
+bob.penup()
+bob.color("Green")
+bob.right(123)
+bob.forward(163)
+bob.pendown()
+bob.width(20)
+number_list = [1,2,3,4,5]
+for number in number_list:
+    bob.left(8.5)
+    bob.forward(8)
+bob.penup()
+bob.color("Black")
+bob.goto(240,-320)
 screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
-
-
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+bob.write('Adan Hubby',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
