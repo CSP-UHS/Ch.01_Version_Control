0084686fd855f36c7dbe720fd9620c47382866e4
maassjax <maassjax@urbandaleschools.com>
2021-09-09 10:18:46 -0500

    added my biography

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:Jaxson Maass
 
 
 
-Define Forking: 
+Define Forking: copying a file onto your github account
 
-Define Cloning: 
+Define Cloning: copying a file onto your own personal computer
 
-Define Branching:
+Define Branching: two different files of the same file for example the master and test thing we did a week ago
 
-Define Committing: 
+Define Committing: making a save on a python file
 
-Define Merging: 
+Define Merging: where the person you pull requested checks your code and brings the code into their code on pycharm
 
-Define Pushing:
+Define Pushing: putting a file from python to our github account
 
-Define Pull Request:
-
+Define Pull Request: submitting code to the developer/master
+try again
 
 '''
--- 1.1_turtorial.py
+++ 1.1_turtorial.py
@@ -12,24 +12,91 @@ yoda=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
 screen.bgcolor('black') # colors the screen
 yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
+yoda.speed(11)  # speed of drawing. Go fast to not waste time.
+yoda.setpos(0,-300)
+yoda.color("#FFFFFF")
+yoda.begin_fill()
+yoda.circle(100)  #snowman body
+yoda.end_fill()
 yoda.penup()
-yoda.setpos(50,185) #right ear
+yoda.setpos(0,-100)#middle body
+yoda.begin_fill()
 yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
+yoda.circle(80)
+yoda.end_fill()
 yoda.penup()
-yoda.setpos(-50,185)  #left ear
+yoda.setpos(0,60)  #head
+yoda.begin_fill()
 yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
+yoda.circle(50)
+yoda.end_fill()
 yoda.penup()
+yoda.goto(0,-50) #snowman buttons
+yoda.color('#000000')
+yoda.begin_fill()
+yoda.pendown()
+yoda.circle(10)
+yoda.end_fill()
+yoda.penup()
+yoda.goto(0,-20)
+yoda.begin_fill()
+yoda.pendown()
+yoda.circle(10)
+yoda.end_fill()
+yoda.penup()
+yoda.goto(0,10)
+yoda.begin_fill()
+yoda.pendown()
+yoda.circle(10)
+yoda.end_fill()
+yoda.penup()
+yoda.goto(-15,110) #eyes
+yoda.pencolor('#000000')
+yoda.pendown()
+yoda.circle(15)
+yoda.penup()
+yoda.goto(-15,115) #pupils
+yoda.pendown()
+yoda.begin_fill()
+yoda.circle(5)
+yoda.end_fill()
+yoda.penup()
+yoda.goto(15,110)
+yoda.pendown()
+yoda.circle(15)
+yoda.penup()
+yoda.goto(15,115) #pupils
+yoda.pendown()
+yoda.begin_fill()
+yoda.circle(5)
+yoda.end_fill()
+yoda.penup()
+yoda.goto(-20,85)
+yoda.pendown()
+yoda.goto(20,85)
+yoda.penup()
+yoda.goto(0,110)
+yoda.pendown() #nose
+yoda.color("#FFA500")
+yoda.begin_fill()
+yoda.goto(40,100)
+yoda.goto(0,90)
+yoda.goto(0,110)
+yoda.end_fill()
+yoda.penup()
+
+
+
+
+
+
+
+
+
 yoda.setpos(200,-300)
 yoda.pendown()
-yoda.pencolor('#00FF00')
+yoda.pencolor('#FFFFFF')
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+yoda.write('Jaxson Maass',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
