566a13b4c920f0ded13d5d663482119551304e7d
earpowe <earpowe@urbandaleschools.com>
2022-09-02 08:56:55 -0500

    Jedi Training Assignment

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Owen Earp
 
 
 
-Define Forking: 
+Define Forking: Forking is when you get a file from an organization and get your own copy of it.
 
-Define Cloning: 
+Define Cloning: When you get a file from GitHub and put it on PyCharm.
 
-Define Branching:
+Define Branching: When you make a whole different section of code within the same program.
 
-Define Committing: 
+Define Committing: A hard save on your code.
 
-Define Merging: 
+Define Merging: When you get 2 or more files and put them together.
 
-Define Pushing:
+Define Pushing: Getting a file from PyCharm back onto GitHub.
 
-Define Pull Request:
+Define Pull Request: Sending a request for someone else to look at your code.
 
 
 '''
