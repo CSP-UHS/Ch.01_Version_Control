49641f00075c6a9a6db02bec9bcb492302e9ad1e
zimmermanaid <zimmermanaid@urbandaleschools.com>
2022-09-02 08:56:30 -0500

    my code

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,22 @@
 '''
-Sign your name:________________
+Sign your name:Aidan Zimmerman
 
 
 
-Define Forking: 
+Define Forking: Making a copy of a file/code, doing this places the file in your account on Git
 
-Define Cloning: 
+Define Cloning: This transfers your coding file from your git account to py so you can code
 
-Define Branching:
+Define Branching: It makes a separate version of your code in the same file but on a different page. Each page can
+then can be edited on its own
 
-Define Committing: 
+Define Committing: Committing saves your code, basically telling the computer this is how things should be.
 
-Define Merging: 
+Define Merging: It is putting two files together
 
-Define Pushing:
+Define Pushing: Puts your code back out to git, updating your previous fork that is there
 
-Define Pull Request:
+Define Pull Request: A pull request allows hermon to see my code and/or merge it with the file
 
 
 '''
