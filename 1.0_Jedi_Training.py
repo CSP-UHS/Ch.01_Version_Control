eda01669f73bd17bf46d40d8c4927ba4217b12bf
sandoval-ordagicben <sandoval-ordagicben@urbandaleschools.com>
2022-09-07 08:10:53 -0500

    finsihed

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:Benjamin Ordagic________________
 
 
 
-Define Forking: 
+Define Forking: Taking a source code from one project and creating a whole new project with it
 
-Define Cloning: 
+Define Cloning: Creating an exact copy of a program
 
-Define Branching:
+Define Branching: Having another version/testing version copyed off of your exact oringal code
 
-Define Committing: 
+Define Committing: To make changes permanent and being able to log them
 
-Define Merging: 
+Define Merging: Combining branches
 
-Define Pushing:
+Define Pushing: Getting your code pushed to git
 
-Define Pull Request:
+Define Pull Request: Your code merged with master code to make sure no problems
 
 
 '''
