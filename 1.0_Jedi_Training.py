2d873e8fe494ce253eec965960225f970dd7a65f
mcinvillecar <mcinvillecar@gmail.com>
2022-09-07 09:09:45 -0500

    done with defs

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:Carter McInville
 
 
 
-Define Forking: 
+Define Forking: Take the original version of the software and create your own new software
 
-Define Cloning: 
+Define Cloning: creating a new copy of software
 
-Define Branching:
+Define Branching: an instruction that tells the computer to run a program
 
-Define Committing: 
+Define Committing: sends latest changes to repository
 
-Define Merging: 
+Define Merging: combines multiple versions of software
 
-Define Pushing:
+Define Pushing: the delivery of information on the web
 
-Define Pull Request:
+Define Pull Request: when a contributor is ready to begin merging
 
 
 '''
