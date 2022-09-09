b44b6836dae1ff5e5e472f775fb42b24d74d2b59
sandoval-ordagicben <sandoval-ordagicben@urbandaleschools.com>
2022-09-09 08:59:18 -0500

    finsihed

--- 1.1_turtorial.py
+++ 1.1_turtorial.py
@@ -10,16 +10,16 @@ Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=
 import turtle
 yoda=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
+screen.bgcolor("white") # colors the screen
 yoda.pensize(3) # width of pen line
 yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
+yoda.color("black")
 yoda.circle(100)  #head
 yoda.penup()
-yoda.setpos(50,185) #right ear
+yoda.setpos(10,60) #right ear
 yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
+yoda.goto(20,75)
+yoda.goto(35,55)
 yoda.penup()
 yoda.setpos(-50,185)  #left ear
 yoda.pendown()
@@ -31,5 +31,5 @@ yoda.pendown()
 yoda.pencolor('#00FF00')
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+yoda.write('Benjamin Ordagic',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
