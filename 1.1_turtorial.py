a8ec6abb1a403d31e1f191f0415c2f4e8870dc9f
donohoand <donohoand@urbandaleschools.com>
2022-09-12 10:27:13 -0500

    turtle

--- 1.1_turtorial.py
+++ 1.1_turtorial.py
@@ -1,14 +1,6 @@
-'''
-Modify the starter code below to create your own cool drawing
-and then Pull Request it to your instructor. Make sure you
-keep the last two lines of code. Your first and last name must be written on your art.
-The last line keeps the window open until you click to close.
 
-Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=turtle
+import turtle
 
-'''
-import turtle
-import turtle
 
 tina = turtle.Turtle()
 
@@ -64,8 +56,7 @@ tina.goto(5,-200)
 tina.goto(0,-200)
 tina.goto(0,200)
 tina.end_fill()
-turtle.exitonclick()
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
-turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
+
+tina.exitonclick()
